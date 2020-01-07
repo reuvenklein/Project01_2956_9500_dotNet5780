cda4b3c21bb126be67532eefe2dfd57b4bcafd80
arielsabag <arielsabagsabag@gmail.com>
2020-01-07 21:05:42 +0200

    Added configuration class and static vars.

---
+++ DAL/BE/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Configuration
+    {
+        
+        public static int guestRequestKey = 1;
+        public static int hostKey = 12;
+        public static int HostingUnitKey = 23;
+        public static int OrderKey = 45;
+
+
+    }
+}
