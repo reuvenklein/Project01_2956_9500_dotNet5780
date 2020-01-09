d4c2e4222811e5342d5b273b007c10dde0238056
arielsabag <44552742+arielsabag@users.noreply.github.com>
2020-01-09 21:58:43 +0200

    Update Order.cs

--- DAL/BE/Order.cs
+++ DAL/BE/Order.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,7 @@ namespace BE
         public string HostingUnitKey { get; set; }
         public string GuestRequestKey { get; set; }
         public string OrderKey { get; set; }
-        public string Status { get; set; }
+        public BE.enum_s.orderStatus Status { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime OrderDate { get; set; }
         public override string ToString()
