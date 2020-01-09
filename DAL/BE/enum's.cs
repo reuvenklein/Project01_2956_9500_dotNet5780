7923c5c06ca1975c9876f3e0992f657c33d10d32
arielsabag <44552742+arielsabag@users.noreply.github.com>
2020-01-09 21:57:09 +0200

    Update enum's.cs

--- DAL/BE/enum's.cs
+++ DAL/BE/enum's.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace BE
 {
-    class enum_s
+    public class enum_s
     {
         public enum regionsInIsrael {All,North,South,Center,Jerusalem}
+        public enum orderStatus { טרם_טופל, נשלח_מייל, נסגר_מחוסר_הענות_של_הלקוח, נסגר_בהיענות_של_הלקוח}
+
     }
 }
